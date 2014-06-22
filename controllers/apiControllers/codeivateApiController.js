6f1b54f1f6750f884d320b977445847aad27cabb
Stephan Williams <stephanwilliams.93@gmail.com>
2014-06-21 20:44:35 -0400

    move logic into separate functions, impelement feching all user info

--- controllers/apiControllers/codeivateApiController.js
+++ controllers/apiControllers/codeivateApiController.js
@@ -1,17 +1,55 @@
 module.exports = function(app, request) {
-    app.get('/actions/getCodeivateUserInfo', function(req, res) {
-        var username = req.query.username;
+    var getSingleUserInfo = function(username, callback) {
         console.log('Getting Codeivate user information for ' + username);
         request('http://codeivate.com/users/' + username + '.json', function(error, response, body) {
             if (error) {
                 throw error;
             }
 
+            callback(body);
+        });
+    };
+
+    // takes a single username or an array of usernames
+    this.getCodeivateUserInfo = function(username, callback) {
+        if (username instanceof Array) {
+            var completedCount = 0;
+            var result = [];
+            // try to make all requests simultaneously...
+            username.forEach(function(element, index, array) {
+                console.log("request " + element);
+                // AFAICT we can only request info one user at a time
+                getSingleUserInfo(element, function(body) {
+                    result[index] = body;
+                    completedCount++;
+                    // ...and callback when the last one completes
+                    if (completedCount === username.length) {
+                        callback('[' + result.join(',') + ']');
+                    }
+                })
+            })
+        } else {
+            getSingleUserInfo(username, callback);
+        }
+    };
+
+    this.getAllCodeviateUserInfo = function(callback) {
+        var usernames = ["swilliams", "camo1018", "ptolemy"];
+        getCodeivateUserInfo(usernames, function(body) {
+            callback(body);
+        });
+    };
+
+    app.get('/actions/getCodeivateUserInfo', function(req, res) {
+        var username = req.query.username;
+        getCodeivateUserInfo(username, function (body) {
             res.json(body);
         });
     });
 
-    app.get('/action/getAllCodeviateUserInfo', function(req, res) {
-        // TODO
+    app.get('/actions/getAllCodeviateUserInfo', function(req, res) {
+        getAllCodeviateUserInfo(function(body) {
+            res.json(body);
+        })
     });
 }
