b163ab3661b21039a7562f1ac5b4a038199c0ee9
Stephan Williams <stephanwilliams.93@gmail.com>
2014-06-21 23:20:50 -0400

    set basic knockout bindings + masonry

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -1,9 +1,26 @@
 /**
  * Created by Paul on 6/21/2014.
  */
+
+ var masonry = new Masonry($('#user-container').get(0), {
+ 	columnWidth: 200
+ });
+
+ function CodeivateUsersViewModel() {
+ 	this.addToMasonry = function(element, index, data) {
+ 		masonry.appended(element);
+ 		console.log('adding');
+ 	};
+ 	this.users = ko.observable([]);
+ }
+
+ var codeivateUsersViewModel = new CodeivateUsersViewModel();
+
 $(function() {
     var params = { username: 'camo1018' };
-    $.get('/actions/getCodeivateUserInfo', params, function(data)  {
-        $('#test').html(data);
+    $.get('/actions/getAllCodeivateUserInfo', params, function(data)  {
+    	codeivateUsersViewModel.users(JSON.parse(data));
     });
 });
+
+ko.applyBindings(codeivateUsersViewModel);
